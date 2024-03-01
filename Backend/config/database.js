25428cbd6440f4f674880ec0455f3571f620d330
Aswanth <aswanthndl@gmail.com>
2024-03-01 19:33:18 +0530

    27.database config changed

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -5,7 +5,7 @@ const connectDatabase=async()=>{
         await mongoose.connect(`${process.env.DB_URL}`)
         .then(()=>console.log("DB successfully connected"));
       } catch (error) {
-        handleError(error);
+        console.log(error);
       }
 }
 
