d8d6a86a47af088f73cf1baac17a1e4feb22d93d
Aswanth <aswanthndl@gmail.com>
2024-03-09 14:14:09 +0530

    53.production deploy

--- Frontend/User/config.js
+++ Frontend/User/config.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://16.16.254.116"
-export const BASE_URL = "http://localhost:3000"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://ecommerce-2-cojb.onrender.com";
