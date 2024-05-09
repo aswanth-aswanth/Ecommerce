edf12d6a6ac54619c85072221bf86780ba71abe6
aswanth <aswanthndl@gmail.com>
2024-05-09 09:36:23 +0530

    Containerized application using docker
    - Created Dockerfile in each application (user, admin, server)
    - Created Dockercompose file in root directory

--- Backend/server.js
+++ Backend/server.js
@@ -1,50 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const path = require('path');
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
 
-const mongooseConnection = require('./config/database');
-const userRoutes = require('./routes/userRoutes.js');
-const adminRoutes = require('./routes/adminRoutes.js');
-const userBuildPath = path.join(__dirname, "../Frontend/User/dist");
-const adminBuildPath=path.join(__dirname,"../Frontend/Admin/dist")
+const mongooseConnection = require("./config/database");
+const userRoutes = require("./routes/userRoutes.js");
+const adminRoutes = require("./routes/adminRoutes.js");
+
 mongooseConnection();
 
-app.use(express.static(userBuildPath));
-app.use(express.static(adminBuildPath));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/uploads', express.static('uploads'));
+app.use("/uploads", express.static("uploads"));
 
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
+
+// Serve static files from the shared volume
+app.use("/user", express.static(path.join(__dirname, "static/user")));
+app.use("/admin", express.static(path.join(__dirname, "static/admin")));
 
 app.get("/adminpanel*", function (req, res) {
-    res.sendFile(
-        path.join(__dirname, "../Frontend/Admin/dist/index.html"),
-        function (err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    );
+  res.sendFile(path.join(__dirname, "static/admin/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 app.get("/*", function (req, res) {
-    res.sendFile(
-        path.join(__dirname, "../Frontend/User/dist/index.html"),
-        function (err) {
-            if (err) {
-                res.status(500).send(err);
-            }
-        }
-    );
+  res.sendFile(path.join(__dirname, "static/user/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
-
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}`));
