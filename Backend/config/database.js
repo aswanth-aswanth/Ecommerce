f5a490881adf1e0fab684e31f38b76532a199f88
aswanth <aswanthndl@gmail.com>
2024-05-13 12:21:22 +0530

    Deployed application using kubernetics

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,12 +1,13 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDatabase=async()=>{
-    try {
-        await mongoose.connect(`${process.env.DB_URL}`)
-        .then(()=>console.log("DB successfully connected"));
-      } catch (error) {
-        console.log("Error connecting Your database : ",error);
-      }
-}
+const connectDatabase = async () => {
+  try {
+    await mongoose
+      .connect(`${process.env.DB_URL}`)
+      .then(() => console.log("DB successfully connected"));
+  } catch (error) {
+    console.log("Error connecting Your database : ", error);
+  }
+};
 
-module.exports=connectDatabase;
+module.exports = connectDatabase;
--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,14 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const path = require("path");
+const logger = require("morgan");
 
 const mongooseConnection = require("./config/database");
 const userRoutes = require("./routes/userRoutes.js");
 const adminRoutes = require("./routes/adminRoutes.js");
-
 mongooseConnection();
 
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,18 +18,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
 app.use("/user", userRoutes);
-
 app.use("/admin", adminRoutes);
 
+console.log("Directory name : ", __dirname);
+console.log("path join user : ", path.join(__dirname, "../user/dist"));
+
+// Serve frontend build files
 app.use(express.static(path.join(__dirname, "../user/dist")));
 app.use(express.static(path.join(__dirname, "../admin/dist")));
 
 app.get("/adminpanel*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../admin/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../admin/dist", "index.html"));
 });
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../user/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../user/dist", "index.html"));
 });
 
 const port = process.env.PORT || 4000;
