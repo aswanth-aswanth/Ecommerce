3550be26b99ed862e0ba740793027aac451c160d
aswanth <aswanthndl@gmail.com>
2024-05-18 20:23:30 +0530

    Make url changes to product details page

--- Frontend/User/src/components/product/Product.jsx
+++ Frontend/User/src/components/product/Product.jsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
-import photo from "../../assets/images/Laptop.png";
-
 import axiosInstance from "../../utils/axiosConfig";
 import { BASE_URL } from "../../../config";
 import { useNavigate, useParams } from "react-router-dom";
 import Specification from "./Specification";
-import Modal from "../common/Modal";
 import InnerImageZoom from "react-inner-image-zoom";
 import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
 import "./styles.css";
@@ -30,7 +27,9 @@ function Product() {
 
   useEffect(() => {
     const fetchDetails = async () => {
-      const response = await axiosInstance.get(`/user/products/${productId}/product`);
+      const response = await axiosInstance.get(
+        `/user/products/${productId}/product`
+      );
       // console.log("RESPONSE : ", response.data);
       setTabs(response.data?.variantIds);
       // console.log("RESPONSE : ", response.data.productDetails);
@@ -41,9 +40,11 @@ function Product() {
       setCategory(response.data.category);
       setIsCartFound(response.data.isCartFound);
       setIsWishlistFound(response.data.isWishlistFound);
-      setSpecification(response.data.productDetails.productDetails.specification);
-      setImage(response.data.productDetails.productDetails.images[0]);
-      setGalleryImages(response.data.productDetails.productDetails.images);
+      setSpecification(
+        response.data.productDetails.productDetails.specification
+      );
+      setImage(response.data.productDetails.productDetails.publicIds[0]);
+      setGalleryImages(response.data.productDetails.productDetails.publicIds);
       setDataRetrieved(true);
     };
 
@@ -79,7 +80,9 @@ function Product() {
 
   const handleAddToWishlist = async (productVariant) => {
     try {
-      const response = await axiosInstance.post(`/user/wishlist`, { productVariant });
+      const response = await axiosInstance.post(`/user/wishlist`, {
+        productVariant,
+      });
       // console.log(response.data);
       setIsWishlistFound((prev) => !prev);
     } catch (error) {
@@ -88,7 +91,9 @@ function Product() {
   };
   const handleRemoveFromWishlist = async (productVariant) => {
     try {
-      const response = await axiosInstance.put(`/user/wishlist`, { productVariant });
+      const response = await axiosInstance.put(`/user/wishlist`, {
+        productVariant,
+      });
       setIsWishlistFound((prev) => !prev);
 
       console.log(response.data);
@@ -150,16 +155,39 @@ function Product() {
         <div className="col-span-10 overflow-hidden col-start-2 col-end-12 lg:col-span-6 h-max border p-4 rounded-md ">
           <div className="border rounded-lg max-w-[350px] h-[310px] p-10 mx-auto">
             <div style={{ height: "210px", overflow: "hidden" }}>
-              <InnerImageZoom src={`${BASE_URL}/uploads/${image}` || photo} alt="Product Image" zoomSrc={`${BASE_URL}/uploads/${image}` || photo} zoomScale={1.8} zoomType="hover" width={350} height={210} enlargeable={true} />
+              <InnerImageZoom
+                src={`${image}` || ""}
+                alt="Product Image"
+                zoomSrc={`${image}` || ""}
+                zoomScale={1.8}
+                zoomType="hover"
+                width={350}
+                height={210}
+                enlargeable={true}
+              />
             </div>
           </div>
 
           <div className="mt-6 w-max   max-w-[24rem] mx-auto whitespace-nowrap overflow-x-scroll no-scrollbar ">
-            <div className="flex gap-4 ">{galleryImages.length > 1 && galleryImages.map((image, index) => <img key={index} className="cursor-pointer shadow-sm object-contain w-16 h-16 border rounded-lg" src={`${BASE_URL}/uploads/${image}`} alt={`Gallery Image ${index + 1}`} onClick={() => handleGalleryImageClick(image)} />)}</div>
+            <div className="flex gap-4 ">
+              {galleryImages.length > 1 &&
+                galleryImages.map((image, index) => (
+                  <img
+                    key={index}
+                    className="cursor-pointer shadow-sm object-contain w-16 h-16 border rounded-lg"
+                    src={`${image}`}
+                    alt={`Gallery Image ${index + 1}`}
+                    onClick={() => handleGalleryImageClick(image)}
+                  />
+                ))}
+            </div>
           </div>
         </div>
         <div className="col-span-10 col-start-2 col-end-12 lg:col-span-6">
-          <h3 className="font-semibold mb-4">{description || "2020 Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray"}</h3>
+          <h3 className="font-semibold mb-4">
+            {description ||
+              "2020 Apple MacBook Pro with Apple M1 Chip (13-inch, 8GB RAM, 256GB SSD Storage) - Space Gray"}
+          </h3>
           <div>
             <div className="flex justify-between text-[#5F6C72]">
               {/* <p>
@@ -180,7 +208,9 @@ function Product() {
           </div>
           {/* ${offer && "line-through"} */}
           {/* {console.log("item ::: ", item)} */}
-          <p className={`text-[#2DA5F3] text-lg font-bold  my-6`}>Price : {item.productDetails?.salePrice || item?.salePrice}₹</p>
+          <p className={`text-[#2DA5F3] text-lg font-bold  my-6`}>
+            Price : {item.productDetails?.salePrice || item?.salePrice}₹
+          </p>
           {/* {offer && (
             <>
               
@@ -196,7 +226,15 @@ function Product() {
           {/* //tabs */}
           <div className="flex">
             {tabs.map((item, index) => (
-              <button key={index} className={`font-bold px-4 py-2 border-b-2 focus:outline-none ${index === activeTab ? "text-[#2DA5F3] border-[#2DA5F3]" : "text-gray-500"}`} onClick={() => handleTabClick(index, item)}>
+              <button
+                key={index}
+                className={`font-bold px-4 py-2 border-b-2 focus:outline-none ${
+                  index === activeTab
+                    ? "text-[#2DA5F3] border-[#2DA5F3]"
+                    : "text-gray-500"
+                }`}
+                onClick={() => handleTabClick(index, item)}
+              >
                 variant {index + 1}
               </button>
             ))}
@@ -222,11 +260,19 @@ function Product() {
           {/* {console.log("ITEM : ", item)} */}
           <div className="flex items-center  h-10 my-6 mt-[60px] gap-4 text-xs font-bold justify-start">
             {dataRetrieved && isCartFound ? (
-              <button onClick={() => navigate("/cart")} className="bg-[#FA8232] text-white  px-16 h-full rounded ">
+              <button
+                onClick={() => navigate("/cart")}
+                className="bg-[#FA8232] text-white  px-16 h-full rounded "
+              >
                 VIEW CART
               </button>
             ) : (
-              <button onClick={() => handleAddToCart(item?.productDetails?._id || item?._id)} className="bg-[#FA8232] text-white  px-16 h-full rounded ">
+              <button
+                onClick={() =>
+                  handleAddToCart(item?.productDetails?._id || item?._id)
+                }
+                className="bg-[#FA8232] text-white  px-16 h-full rounded "
+              >
                 ADD TO CART
               </button>
             )}
@@ -234,16 +280,30 @@ function Product() {
           <div
             onClick={() => {
               if (isWishlistFound) {
-                handleRemoveFromWishlist(item?.productDetails?._id || item?._id);
+                handleRemoveFromWishlist(
+                  item?.productDetails?._id || item?._id
+                );
               } else {
                 handleAddToWishlist(item?.productDetails?._id || item?._id);
               }
             }}
             className="flex items-center justify-start w-max  gap-2 mt-[40px] cursor-pointer"
           >
-            <svg width="22" height="22" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M16 27C16 27 3.5 20 3.5 11.5C3.5 9.99736 4.02062 8.54113 4.97328 7.37907C5.92593 6.21702 7.25178 5.42092 8.72525 5.12623C10.1987 4.83154 11.7288 5.05645 13.0551 5.76271C14.3814 6.46897 15.4221 7.61295 16 9C16.5779 7.61295 17.6186 6.46897 18.9449 5.76271C20.2712 5.05645 21.8013 4.83154 23.2748 5.12623C24.7482 5.42092 26.0741 6.21702 27.0267 7.37907C27.9794 8.54113 28.5 9.99736 28.5 11.5C28.5 20 16 27 16 27Z" fill={isWishlistFound ? "red" : "white"} />
-              <path d="M16 27C16 27 3.5 20 3.5 11.5C3.5 9.99736 4.02062 8.54113 4.97328 7.37907C5.92593 6.21702 7.25178 5.42092 8.72525 5.12623C10.1987 4.83154 11.7288 5.05645 13.0551 5.76271C14.3814 6.46897 15.4221 7.61295 16 9C16.5779 7.61295 17.6186 6.46897 18.9449 5.76271C20.2712 5.05645 21.8013 4.83154 23.2748 5.12623C24.7482 5.42092 26.0741 6.21702 27.0267 7.37907C27.9794 8.54113 28.5 9.99736 28.5 11.5C28.5 20 16 27 16 27Z" fill={isWishlistFound ? "red" : "gray"} />
+            <svg
+              width="22"
+              height="22"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M16 27C16 27 3.5 20 3.5 11.5C3.5 9.99736 4.02062 8.54113 4.97328 7.37907C5.92593 6.21702 7.25178 5.42092 8.72525 5.12623C10.1987 4.83154 11.7288 5.05645 13.0551 5.76271C14.3814 6.46897 15.4221 7.61295 16 9C16.5779 7.61295 17.6186 6.46897 18.9449 5.76271C20.2712 5.05645 21.8013 4.83154 23.2748 5.12623C24.7482 5.42092 26.0741 6.21702 27.0267 7.37907C27.9794 8.54113 28.5 9.99736 28.5 11.5C28.5 20 16 27 16 27Z"
+                fill={isWishlistFound ? "red" : "white"}
+              />
+              <path
+                d="M16 27C16 27 3.5 20 3.5 11.5C3.5 9.99736 4.02062 8.54113 4.97328 7.37907C5.92593 6.21702 7.25178 5.42092 8.72525 5.12623C10.1987 4.83154 11.7288 5.05645 13.0551 5.76271C14.3814 6.46897 15.4221 7.61295 16 9C16.5779 7.61295 17.6186 6.46897 18.9449 5.76271C20.2712 5.05645 21.8013 4.83154 23.2748 5.12623C24.7482 5.42092 26.0741 6.21702 27.0267 7.37907C27.9794 8.54113 28.5 9.99736 28.5 11.5C28.5 20 16 27 16 27Z"
+                fill={isWishlistFound ? "red" : "gray"}
+              />
             </svg>
 
             <p>Add to wishlist</p>
@@ -252,10 +312,20 @@ function Product() {
       </div>
       <div className=" mt-16 max-w-[940px] text-sm mx-auto mb-10">
         <div className="flex justify-center gap-20 border items-center h-16">
-          <button onClick={() => setIsSpecification(false)} className={`${!isSpecification && "border-b-4 border-b-[#FA8232]"} h-full`}>
+          <button
+            onClick={() => setIsSpecification(false)}
+            className={`${
+              !isSpecification && "border-b-4 border-b-[#FA8232]"
+            } h-full`}
+          >
             DESCRIPTION
           </button>
-          <button onClick={() => setIsSpecification(true)} className={`${isSpecification && "border-b-4 border-b-[#FA8232] "} h-full`}>
+          <button
+            onClick={() => setIsSpecification(true)}
+            className={`${
+              isSpecification && "border-b-4 border-b-[#FA8232] "
+            } h-full`}
+          >
             SPECIFICATION
           </button>
         </div>
